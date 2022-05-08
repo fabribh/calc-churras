9c129fdcb1dcf9550f98a77703a31ebcad4e067f
Fabio Ribeiro <si.fabioribeiro@gmail.com>
2022-05-08 10:09:16 -0300

    Alterando o layout para atender os requisitos do trabalho

--- app/src/main/java/com/fabribh/churrascocalculator/MainActivity.java
+++ app/src/main/java/com/fabribh/churrascocalculator/MainActivity.java
@@ -3,12 +3,66 @@ package com.fabribh.churrascocalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinnerItens;
+    private EditText editTextNome, editTextPhone;
+    private RadioGroup radioGroup;
+    private CheckBox checkBoxSuco, checkBoxRefri, checkBoxCerveja,
+            checkBoxBoi, checkBoxFrango, checkBoxPorco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinnerItens = findViewById(R.id.spinnerItens);
+        editTextNome = findViewById(R.id.editTextNome);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        radioGroup = findViewById(R.id.radioGroup);
+
+        checkBoxSuco = findViewById(R.id.checkBoxSuco);
+        checkBoxRefri = findViewById(R.id.checkBoxRefri);
+        checkBoxCerveja = findViewById(R.id.checkBoxCerveja);
+        checkBoxBoi = findViewById(R.id.checkBoxBoi);
+        checkBoxFrango = findViewById(R.id.checkBoxFrango);
+        checkBoxPorco = findViewById(R.id.checkBoxPorco);
+
+        populateSpinner();
+    }
+
+    private void populateSpinner() {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add(getString(R.string.feminino));
+        list.add(getString(R.string.masculino));
+
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+
+        spinnerItens.setAdapter(arrayAdapter);
+    }
+
+    public void limparCampos(View view){
+        editTextPhone.setText(null);
+        editTextNome.setText(null);
+
+        radioGroup.clearCheck();
+
+        checkBoxPorco.setSelected(false);
+        checkBoxBoi.setSelected(false);
+        checkBoxFrango.setSelected(false);
+        checkBoxSuco.setSelected(false);
+        checkBoxCerveja.setSelected(false);
+        checkBoxRefri.setSelected(false);
     }
 }
